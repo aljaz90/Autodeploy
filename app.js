5999a5f1b17e321965ce056b19dc1fd3dbe8eff0
server@home <aljaz1.kern@gmail.com>
2022-09-06 16:53:59 +0200

    Add additional config options

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express       = require("express"),
-      app           = express(),
-      bodyParser    = require("body-parser"),
-      crypto        = require("crypto"),
-      { execSync }  = require('child_process'),
-      PORT	        = 5000,
-      GITHUB_SECRET = require("./key.js"),
-      sigHeaderName = 'X-Hub-Signature-256',
-      sigHashAlg    = 'sha256';
+const express                = require("express"),
+      app                    = express(),
+      bodyParser             = require("body-parser"),
+      crypto                 = require("crypto"),
+      { execSync }           = require('child_process'),
+      SIG_HEADER_NAME        = 'X-Hub-Signature-256',
+      SIG_HASH_ALG           = 'sha256',
+      // Config variables
+      PORT	                 = 5000, // Set the port on which it will listen for GitHub webhook requests
+      GITHUB_SECRET          = "",   // Set your GitHub webhook secret
+      PATH_TO_SHELL_SCRIPT   = "",   // Set path to the shell script which will be executed
+      RUN_SCRIPT_WITH_SUDO   = false,
+      WATCH_BRANCHES         = [];   // Enter branch names to listen only for events on those brances or leave empty to listen on all branches
     
 app.use(bodyParser.json({
     verify: (req, res, buf, encoding) => {
@@ -22,9 +26,9 @@ function verifySecret(req, res, next) {
         return res.end();
     }
 
-    const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8');
-    const hmac = crypto.createHmac(sigHashAlg, GITHUB_SECRET);
-    const digest = Buffer.from(sigHashAlg + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8');
+    const sig = Buffer.from(req.get(SIG_HEADER_NAME) || '', 'utf8');
+    const hmac = crypto.createHmac(SIG_HASH_ALG, GITHUB_SECRET);
+    const digest = Buffer.from(SIG_HASH_ALG + '=' + hmac.update(req.rawBody).digest('hex'), 'utf8');
     if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
         console.log("Hashes don't match");
         return res.end();
@@ -35,14 +39,20 @@ function verifySecret(req, res, next) {
 
 app.post("/deploy", verifySecret, (req, res) => {
     try {
-        if (req.body.ref.includes("/production")  && req.body.head_commit?.author?.name !== "server@home") {
+        let branch = req.body.ref.split("/")[2];
+        if (WATCH_BRANCHES.length === 0 || WATCH_BRANCHES.includes(branch)) {
             console.log("Running script")
-            execSync("sudo sh /home/aljaz-adm/Documents/LearnZone/autodeploy.sh");
-            console.log("Script executed successfully")
+            let commandToRun = `sh ${PATH_TO_SHELL_SCRIPT}`;
+            if (RUN_SCRIPT_WITH_SUDO) {
+                commandToRun = "sudo " + commandToRun;
+            }
+    
+            execSync(commandToRun);
+            console.log("Script executed successfully");
         }
     } 
     catch (error) {
-        console.log("Error while executing script");
+        console.log("An error occured while executing the script");
         console.log(error);
     }
 
